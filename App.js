98e75b4b9c48208c82917668d4d73293c237eedf
SrDudu666 <112430322+SrDudu666@users.noreply.github.com>
2024-02-19 15:19:54 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {exibirHorario} from './components/Horario'
+import {exibirHorario} from './components/Horario';
 
 function App() {
   return (
